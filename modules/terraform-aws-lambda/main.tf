a2c23cf5e4c5f927cffb790a74296988950d7ee3
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-05 12:56:13 +0530

    fix

--- modules/terraform-aws-lambda/main.tf
+++ modules/terraform-aws-lambda/main.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
 
-
+   layers                         = ["arn:aws:lambda:ap-south-1:770693421928:layer:Klayers-p39-pillow:1"]
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
