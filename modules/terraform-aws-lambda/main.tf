08a2044f4f90a77f448531f1dc882b90fba0be5a
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-05 10:23:58 +0530

    fix

--- modules/terraform-aws-lambda/main.tf
+++ modules/terraform-aws-lambda/main.tf
@@ -19,14 +19,13 @@ resource "aws_iam_role" "iam_for_lambda" {
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "${path.module}/lambda.py"
-
   output_path = "lambda_function_payload.zip"
 }
 
 
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "lambda_function"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.lambda_handler"
 
