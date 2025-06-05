13e23d05dac2683a232124636933ed45debed8f6
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-05 10:30:04 +0530

    fix

--- modules/terraform-aws-lambda/main.tf
+++ modules/terraform-aws-lambda/main.tf
@@ -40,7 +40,7 @@ resource "aws_sns_topic" "user_updates" {
 
 
 resource "aws_sns_topic_subscription" "user_updates_email" {
-  topic_arn = aws_sns_topic.user_updates.arn
-  protocol  = "email"
-  endpoint  = "prakashvelusamy1999@gmail.com"
+topic_arn = aws_sns_topic.user_updates.arn 
+protocol = "email"
+endpoint = "prakashvelusamy1999@gmail.com"
 }
