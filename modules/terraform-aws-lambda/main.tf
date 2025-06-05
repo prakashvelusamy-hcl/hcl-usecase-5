34fcb197a805822f181eaa6050b2a96cecaff3eb
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-05 11:38:58 +0530

    fix

--- modules/terraform-aws-lambda/main.tf
+++ modules/terraform-aws-lambda/main.tf
@@ -11,6 +11,7 @@ resource "aws_lambda_function" "lambda" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
 
+
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
